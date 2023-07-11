64b5c888ce68b153184b491602da39d88015ca6a
Adrian Ferrari <sdadrian@gmail.com>
2023-07-11 01:37:45 -0300

    #13-style (client): add landing page default style

--- client/src/pages/Home/Welcome/index.tsx
+++ client/src/pages/Home/Welcome/index.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 export default function Welcome() {
+    useEffect(() => {
+        const root = document.getElementById("root");
+        root?.style.setProperty("--text-primary", "white");
+        root?.style.setProperty("--text-secondary", "white");
+        root?.style.setProperty("--primary", "black");
+        root?.style.setProperty("--secondary", "black");
+        root?.style.setProperty("--tertiary", "black");
+    }, []);
+
     return (
         <main>
             <h1>Welcome!&#128512;</h1>
