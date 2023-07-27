e21b10a5b784c0a1f1abc2f0fee62c7a377c7c11
NicolasFontana <nico.fontana12@gmail.com>
2023-07-27 19:11:44 -0300

    refactor(server): get cart by user id and jwt auth

--- server/src/filters/jwtAuth.ts
+++ server/src/filters/jwtAuth.ts
@@ -5,16 +5,16 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 @Injectable({ lifeTime: DependencyLifeTime.Scoped })
 export class JWTAuth implements IFilter {
     async beforeExecute(httpContext: HttpContext) {
-        const token = httpContext.request.header("x-auth");
         try {
+            const token = httpContext.request.header("x-auth");
             if (!token) throw new Error("Token not found");
-            const entity = httpContext.request.body;
-            httpContext.request.body = {};
-            httpContext.request.body.entity = entity;
             const decodedToken = jwt.verify(token.replace("Bearer ", ""), process.env.SHOPPY__ACCESS_TOKEN) as JwtPayload;
             delete decodedToken.iat;
             delete decodedToken.exp;
-            httpContext.request.body.decodedToken = decodedToken;
+            httpContext.request.body = {
+                entity: httpContext.request.body,
+                decodedToken: decodedToken,
+            };
         } catch (error) {
             httpContext.response.clearCookie("token");
             httpContext.response.status(401).json({
