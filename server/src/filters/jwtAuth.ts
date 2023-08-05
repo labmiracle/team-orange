bdbabcd074f9f0a41c8efb9d52063f9fb5706a42
Adrian Ferrari <sdadrian@gmail.com>
2023-08-04 21:26:34 -0300

    #50-fix(server): check database for role
    Co-authored-by: Nicolás Fontana <NicolasFontana@users.noreply.github.com>

--- server/src/filters/jwtAuth.ts
+++ server/src/filters/jwtAuth.ts
@@ -1,58 +1,49 @@
 import { Injectable, DependencyLifeTime } from "@miracledevs/paradigm-web-di";
 import { IFilter, HttpContext } from "@miracledevs/paradigm-express-webapi";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { MySqlConnection } from "../core/mysql/mysql.connection";
+import { BatchDbCommand } from "../core/repositories/commands/batch.command";
+import { RowDataPacket } from "mysql2/promise";
 
 @Injectable({ lifeTime: DependencyLifeTime.Scoped })
 export class JWTAuth implements IFilter {
     async beforeExecute(httpContext: HttpContext) {
-        try {
-            const token = httpContext.request.header("x-auth");
-            if (!token) throw new Error("Token not found");
-            const decodedToken = jwt.verify(token, process.env.SHOPPY__ACCESS_TOKEN) as JwtPayload;
-            delete decodedToken.iat;
-            delete decodedToken.exp;
-            httpContext.request.body = {
-                entity: httpContext.request.body,
-                decodedToken: decodedToken,
-            };
-        } catch (error) {
-            httpContext.response.status(401).json({
-                message: error.message,
-                data: null,
-                error: true,
-            });
-        }
+        const token = httpContext.request.header("x-auth");
+        if (!token) throw new Error("Token not found");
+        const decodedToken = jwt.verify(token, process.env.SHOPPY__ACCESS_TOKEN) as JwtPayload;
+        delete decodedToken.iat;
+        delete decodedToken.exp;
+        httpContext.request.body = {
+            entity: httpContext.request.body,
+            decodedToken: decodedToken,
+        };
     }
 }
 
 @Injectable({ lifeTime: DependencyLifeTime.Scoped })
 export class isAdmin implements IFilter {
+    protected batch: BatchDbCommand;
+    constructor(protected readonly connection: MySqlConnection) {
+        this.batch = new BatchDbCommand(connection);
+    }
+
     async beforeExecute(httpContext: HttpContext) {
-        try {
-            const rol = httpContext.request.body.decodedToken.rol;
-            if (rol !== "Admin") throw new Error("Unauthorized");
-        } catch (error) {
-            httpContext.response.status(401).json({
-                message: error.message,
-                data: null,
-                error: true,
-            });
-        }
+        const { id } = httpContext.request.body.decodedToken;
+        const [rows] = await this.connection.connection.execute<RowDataPacket[]>("SELECT * FROM user WHERE id = ?", [id]);
+        if (rows[0].rol !== "Admin") throw new Error("Unauthorized");
     }
 }
 
 @Injectable({ lifeTime: DependencyLifeTime.Scoped })
 export class isManager implements IFilter {
+    protected batch: BatchDbCommand;
+    constructor(protected readonly connection: MySqlConnection) {
+        this.batch = new BatchDbCommand(connection);
+    }
+
     async beforeExecute(httpContext: HttpContext) {
-        try {
-            const rol = httpContext.request.body.decodedToken.rol;
-            if (rol !== "Manager") throw new Error("Unauthorized");
-        } catch (error) {
-            httpContext.response.status(401).json({
-                message: error.message,
-                data: null,
-                error: true,
-            });
-        }
+        const { id } = httpContext.request.body.decodedToken;
+        const [rows] = await this.connection.connection.execute<RowDataPacket[]>("SELECT * FROM user WHERE id = ?", [id]);
+        if (rows[0].rol !== "Manager") throw new Error("Unauthorized");
     }
 }
