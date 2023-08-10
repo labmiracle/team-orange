228f5cb24cfa84c6be194bc4da36fd8d09c108d0
Adrian Ferrari <sdadrian@gmail.com>
2023-08-09 22:22:14 -0300

    #11-feat(server): add filter for sales items
    Co-authored-by: Nicolás Fontana <NicolasFontana@users.noreply.github.com>

--- server/src/filters/product.filter.ts
+++ server/src/filters/product.filter.ts
@@ -1,6 +1,6 @@
 import { Injectable, DependencyLifeTime } from "@miracledevs/paradigm-web-di";
 import { IFilter, HttpContext } from "@miracledevs/paradigm-express-webapi";
-import { productSchema, productArray } from "../models/schemas/product.schema";
+import { productSchema, productArray, productSaleArray } from "../models/schemas/product.schema";
 import { MySqlConnection } from "../core/mysql/mysql.connection";
 import { BatchDbCommand } from "../core/repositories/commands/batch.command";
 import { RowDataPacket } from "mysql2/promise";
@@ -20,6 +20,14 @@ export class ProductFilter implements IFilter {
 export class ProductArrayFilter implements IFilter {
     async beforeExecute(httpContext: HttpContext): Promise<void> {
         const { error } = productArray.validate(httpContext.request.body);
+        if (error) throw new Error(error.details[0].message);
+    }
+}
+
+@Injectable({ lifeTime: DependencyLifeTime.Scoped })
+export class ProductSaleArrayFilter implements IFilter {
+    async beforeExecute(httpContext: HttpContext): Promise<void> {
+        const { error } = productSaleArray.validate(httpContext.request.body);
         if (error) throw new Error(error.details[0].message);
     }
 }
