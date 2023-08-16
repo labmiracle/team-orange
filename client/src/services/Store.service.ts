ba9db9c040c1449a1e371a7d980c3737dc2b3e4e
Adrian Ferrari <sdadrian@gmail.com>
2023-08-16 01:29:41 -0300

    chore(client): add types to query
    Co-authored-by: Nicolás Fontana <NicolasFontana@users.noreply.github.com>

--- client/src/services/Store.service.ts
+++ client/src/services/Store.service.ts
@@ -1,10 +1,12 @@
+import { AxiosResponse } from "axios";
 import { baseEndpoints } from "../endpoints";
+import { LoaderResponse, StoreType, StoreName } from "../types";
 import Fetcher from "./Fetcher";
 
 export class StoreService {
     async getStoreNames() {
         try {
-            const response = await Fetcher.query(baseEndpoints.stores.names, {
+            const response = await Fetcher.query<AxiosResponse<LoaderResponse<StoreName>>>(baseEndpoints.stores.names, {
                 method: "GET",
             });
             const names = response.data.data;
@@ -16,9 +18,12 @@ export class StoreService {
 
     async getStore(storeId: number) {
         try {
-            const response = await Fetcher.query(`${baseEndpoints.stores.store}/${storeId}`, {
-                method: "GET",
-            });
+            const response = await Fetcher.query<AxiosResponse<LoaderResponse<StoreType>>>(
+                `${baseEndpoints.stores.store}/${storeId}`,
+                {
+                    method: "GET",
+                }
+            );
 
             const store = response.data.data;
 
