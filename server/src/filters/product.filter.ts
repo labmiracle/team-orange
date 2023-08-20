c9b3d3d1d7ecb5ff864e0db56a0ed280c4f6f87c
Adrian Ferrari <sdadrian@gmail.com>
2023-08-20 15:49:44 -0300

    #11-fix(server): filter using query instead of repos

--- server/src/filters/product.filter.ts
+++ server/src/filters/product.filter.ts
@@ -4,9 +4,11 @@ import { productSchema, productArray, productSaleArray } from "../models/schemas
 import { MySqlConnection } from "../core/mysql/mysql.connection";
 import { BatchDbCommand } from "../core/repositories/commands/batch.command";
 import { RowDataPacket } from "mysql2/promise";
+import { ProductInterface, ProductSaleInterface } from "../models/product";
+import { ProductDBRepository } from "../repositories/productDB.repository";
 
 /**
- * Requires a mysql connection from the connection pool for the ongoing request.
+ * Validate product of type {@link ProductInterface}
  */
 @Injectable({ lifeTime: DependencyLifeTime.Scoped })
 export class ProductFilter implements IFilter {
@@ -16,6 +18,9 @@ export class ProductFilter implements IFilter {
     }
 }
 
+/**
+ * Validate an array of products of type {@link ProductInterface}
+ */
 @Injectable({ lifeTime: DependencyLifeTime.Scoped })
 export class ProductArrayFilter implements IFilter {
     async beforeExecute(httpContext: HttpContext): Promise<void> {
@@ -24,6 +29,9 @@ export class ProductArrayFilter implements IFilter {
     }
 }
 
+/**
+ * Validate product of type {@link ProductSaleInterface}
+ */
 @Injectable({ lifeTime: DependencyLifeTime.Scoped })
 export class ProductSaleArrayFilter implements IFilter {
     async beforeExecute(httpContext: HttpContext): Promise<void> {
@@ -32,20 +40,20 @@ export class ProductSaleArrayFilter implements IFilter {
     }
 }
 
+/**
+ * Check if the product to be modified belong to the manager's store
+ */
 @Injectable({ lifeTime: DependencyLifeTime.Scoped })
 export class authProductFilter implements IFilter {
     protected batch: BatchDbCommand;
-    constructor(protected readonly connection: MySqlConnection) {
+    constructor(protected readonly connection: MySqlConnection, private productDBRepo: ProductDBRepository) {
         this.batch = new BatchDbCommand(connection);
     }
 
     async beforeExecute(httpContext: HttpContext): Promise<void> {
         const { id: idUser } = JSON.parse(httpContext.request.header("x-auth"));
         const { id: idProduct } = httpContext.request.body;
-        const [rowsP] = await this.connection.connection.execute<RowDataPacket[]>(
-            "SELECT * FROM product p JOIN store s ON s.id = p.storeId WHERE managerId = ? AND p.id = ?",
-            [idUser, idProduct]
-        );
-        if (!rowsP || rowsP.length === 0) throw new Error("Unauthorized Store");
+        const idManager = await this.productDBRepo.getManager(idProduct);
+        if (idManager !== idUser) throw new Error("Unauthorized Store");
     }
 }
