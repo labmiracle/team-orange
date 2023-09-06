84802f5e3179144702aed1b7e2f4e970badf1f05
NicolasFontana <nico.fontana12@gmail.com>
2023-09-06 17:26:29 -0300

    feat(client): add a button to return to the homepage and style changes are made
    Co-authored-by: Adrian Ferrari <sdadrian@gmail.com>

--- client/src/pages/Error/index.tsx
+++ client/src/pages/Error/index.tsx
@@ -1,16 +1,21 @@
 import { useRouteError } from "react-router-dom";
+import styles from "./index.module.css";
+import { Link } from "../../components/ui/Link";
 
 export function ErrorPage() {
     const error = useRouteError() as Error;
     console.error(error);
 
     return (
-        <div id="error-page">
+        <div className={styles.error_container}>
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.&#128532;</p>
             <p>
                 <i>{error.message}</i>
             </p>
+            <Link to="/" className={styles.link_home}>
+                Go to homepage
+            </Link>
         </div>
     );
 }
