d6ba71014284b22133c3b46141dbed34bf6165e2
NicolasFontana <nico.fontana12@gmail.com>
2023-09-18 21:31:36 -0300

    #70-feat(client): add container to loading spinner
    Co-authored-by: Adrian Ferrari <sdadrian@gmail.com>

--- client/src/pages/LoadingSpinner/index.tsx
+++ client/src/pages/LoadingSpinner/index.tsx
@@ -1,12 +1,11 @@
 import styles from "./loader.module.css";
 
-/* export default function Loader() {
-    return (
+export default function Loader({ container = false }: { container?: boolean }) {
+    return container ? (
         <div className={styles.container}>
             <div className={styles.lds_dual_ring}></div>
         </div>
+    ) : (
+        <div className={styles.lds_dual_ring}></div>
     );
-} */
-export default function Loader() {
-    return <div className={styles.lds_dual_ring}></div>;
 }
